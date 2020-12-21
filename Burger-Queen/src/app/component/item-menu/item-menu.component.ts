3d59a2cc3ab017387da35c18904774ba3bb89f4d
consuelogoche-1994 <consuelo.goche@gmail.com>
2020-12-21 12:24:02 -0500

    finish boilerplate

---
+++ Burger-Queen/src/app/component/item-menu/item-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-menu',
+  templateUrl: './item-menu.component.html',
+  styleUrls: ['./item-menu.component.scss']
+})
+export class ItemMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
