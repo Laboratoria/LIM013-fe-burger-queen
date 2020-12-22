6af9b585ca90ea35f7c8a22a9aecaefd7004055e
KatyLuHT <katyhuamanitito1@gmail.com>
2020-12-21 19:39:04 -0500

    creando component resumenItem y menu2

--- Burger-Queen/src/app/app.module.ts
+++ Burger-Queen/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
 import { ItemMenuComponent } from './component/item-menu/item-menu.component';
-import { ResumenItemComponent } from './resumen-item/resumen-item.component';
+import { ResumenItemComponent } from './component/resumen-item/resumen-item.component';
 
 
 @NgModule({
