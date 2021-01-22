4a8d209816759cea830c0ef83326f18d82aca487
consuelogoche-1994 <consuelo.goche@gmail.com>
2021-01-22 01:43:21 -0500

    Upload project to firebase hosting - close#32

--- Burger-Queen/src/environments/environment.prod.ts
+++ Burger-Queen/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase :{
+    apiKey: "AIzaSyCMLDmvd1lotOkyLlW8d_NParCwZHZGX1s",
+    authDomain: "burgerqueen-1a4e9.firebaseapp.com",
+    projectId: "burgerqueen-1a4e9",
+    storageBucket: "burgerqueen-1a4e9.appspot.com",
+    messagingSenderId: "1059758145823",
+    appId: "1:1059758145823:web:9b62095694d1be37b8b02a",
+    measurementId: "G-KMXQ4DB26V"
+  }
 };
